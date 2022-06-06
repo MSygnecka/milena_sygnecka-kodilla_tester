ebbb6fd6913bc6e0d276f27e34ddb3af005f021b
Milena Sygnecka <milena.sygnecka@wp.pl>
2022-06-06 22:24:44 +0200

    HalloWorld

---
+++ src/main/java/HalloWorld.java
@@ -0,0 +1,8 @@
+public class HalloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Milena");
+        System.out.println("37");
+        System.out.println("S");
+    }
+    }
