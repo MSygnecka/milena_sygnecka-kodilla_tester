e40e2ec3cc59e699ef358ce9e4b2ce2d8568b183
Milena Sygnecka <milena.sygnecka@wp.pl>
2022-06-07 21:32:26 +0200

    MainExtended

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Wiktoria";
+        int age = 25;
+        int weight = 77;
+        double height = 1.67;
+        char bodyType = 'A';
+        boolean wantLoseWeight = true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(height);
+        System.out.println(bodyType);
+        System.out.println(wantLoseWeight);
+
+    }
+}
