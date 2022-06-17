1f63c177f2a189817ef87321a4d457465b43576c
Milena Sygnecka <milena.sygnecka@wp.pl>
2022-06-17 18:15:10 +0200

    Grades

---
+++ src/main/java/Grades.java
@@ -0,0 +1,38 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int lastGrade() {
+        if (this.size == 0) {
+            return this.size;
+        } else {
+            return this.grades [this.size - 1];
+        }
+    }
+
+    public int averageGrade() {
+        int sum = 0;
+        int average = 0;
+        for (int i = 0; i < this.size; i++) {
+            sum = sum + this.grades[i];
+            average = sum / this.size;
+
+            return average;
+
+        }
+        return sum;
+    }
+}
