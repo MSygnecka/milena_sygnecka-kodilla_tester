2fd623028eca0de6ae3ab436480991650fc638d0
Milena Sygnecka <milena.sygnecka@wp.pl>
2022-06-29 06:54:43 +0200

    abstracts.Person - poprawiona

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/abstracts/homework/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/abstracts/homework/Person.java
@@ -4,32 +4,32 @@ public class Person {
 
     public String firstName;
     public int age;
-    public String job;
+    public Job job;
 
-    public Person(String firstName, int age, String job) {
+    public Person(String firstName, int age, Job job) {
         this.firstName = firstName;
         this.age = age;
         this.job = job;
     }
     public static void main(String[] args) {
-        Person person1 = new Person("Anna", 22, "nurse");
-        Person person2 = new Person("Kate", 31, "teacher");
-        Person person3 = new Person("Jon", 29, "machanic");
-
         Job nurse = new Nurse(4000, "responsibilities nurse");
-        String nurseResponsibilities = nurse.responsibilities;
+        Job teacher = new Teacher(3000, "responsibilities teacher");
+        Job mechanic = new Mechanic(5000, "responsibilities mechanic");
+        Person person1 = new Person("Anna", 22, nurse);
+        Person person2 = new Person("Kate", 31, teacher);
+        Person person3 = new Person("Jon", 29, mechanic);
+
+        String person1Responsibilities = nurse.responsibilities;
         int nurseSalary = nurse.salary;
 
-        Job teacher = new Teacher(3000, "responsibilities teacher");
-        String teacherResponsibilities = teacher.responsibilities;
-        int teacherSalary = nurse.salary;
+        String person2Responsibilities = teacher.responsibilities;
+        int teacherSalary = teacher.salary;
 
-        Job mechanic = new Mechanic(5000, "responsibilities mechanic");
-        String mechanicResponsibilities = mechanic.responsibilities;
+        String person3Responsibilities = mechanic.responsibilities;
         int mechanicSalary = mechanic.salary;
 
-        System.out.println(person1.firstName + " " + person1.age + " " + person1.job + " " + nurseResponsibilities);
-        System.out.println(person2.firstName + " " + person2.age + " " + person2.job + " " + teacherResponsibilities);
-        System.out.println(person3.firstName + " " + person3.age + " " + person3.job + " " + mechanicResponsibilities);
+        System.out.println(person1.firstName + " " + person1.age + " " + person1.job + " " + person1Responsibilities);
+        System.out.println(person2.firstName + " " + person2.age + " " + person2.job + " " + person2Responsibilities);
+        System.out.println(person3.firstName + " " + person3.age + " " + person3.job + " " + person3Responsibilities);
     }
 }
