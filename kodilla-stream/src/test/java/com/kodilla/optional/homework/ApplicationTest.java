cb731d862d71af4cbae4b54104b37ec9ff840acf
Milena Sygnecka <milena.sygnecka@wp.pl>
2022-08-05 15:02:34 +0200

    zadanie 6.2. Typ Optional

---
+++ kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTest.java
@@ -0,0 +1,34 @@
+package com.kodilla.optional.homework;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @Test
+    public void testApplicationIfTeacherIsNotNull() {
+        Application application = new Application();
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Monica Geller", new Teacher("Ross Geller")));
+        students.add(new Student("Rachel Green", null));
+        students.add(new Student("Chandler Bing", new Teacher("Mike Hannigan")));
+        students.add(new Student("Joey Tribbiani", new Teacher("Phoebe Buffay")));
+        students.add(new Student("Gunther", null));
+
+        for(Student result : students) {
+
+            Optional<Teacher> optionalTeacher = Optional.ofNullable(result.getTeacher());
+
+            Teacher name = optionalTeacher.orElse(new Teacher(("<undefined>")));
+
+
+            assertEquals(result.getName(),students);
+            }
+        }
+}
