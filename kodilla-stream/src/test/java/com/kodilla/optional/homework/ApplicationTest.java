52aa1c23ca4d182981b6766f0482e38959096247
Milena Sygnecka <milena.sygnecka@wp.pl>
2022-08-08 11:22:36 +0200

    zadanie 6.2 - Typ Optional - ApplicationTest

--- kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTest.java
+++ kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTest.java
@@ -16,10 +16,8 @@ class ApplicationTest {
 
         List<Student> students = new ArrayList<>();
         students.add(new Student("Monica Geller", new Teacher("Ross Geller")));
-        students.add(new Student("Rachel Green", null));
         students.add(new Student("Chandler Bing", new Teacher("Mike Hannigan")));
         students.add(new Student("Joey Tribbiani", new Teacher("Phoebe Buffay")));
-        students.add(new Student("Gunther", null));
 
         for(Student result : students) {
 
@@ -27,7 +25,24 @@ class ApplicationTest {
 
             Teacher name = optionalTeacher.orElse(new Teacher(("<undefined>")));
 
-            assertEquals(result.getName(),students);
+            assertEquals(result.getTeacher().getName(),name.getName());
             }
         }
+    @Test
+    public void testApplicationIfTeacherIsNull() {
+        Application application = new Application();
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Rachel Green", null));
+        students.add(new Student("Gunther", null));
+
+        for (Student result : students) {
+
+            Optional<Teacher> optionalTeacher = Optional.ofNullable(result.getTeacher());
+
+            Teacher name = optionalTeacher.orElse(new Teacher(("<undefined>")));
+
+            assertEquals("<undefined>", name.getName());
+        }
+    }
 }
