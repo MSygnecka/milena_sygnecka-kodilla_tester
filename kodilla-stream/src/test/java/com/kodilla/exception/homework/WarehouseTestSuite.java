b0d92b1825fe1ac585940849dbc4ece6d52d5e8c
Milena Sygnecka <milena.sygnecka@wp.pl>
2022-08-10 15:38:18 +0200

    zadanie 6.5. Wyjtki a testy - testy

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -14,4 +14,16 @@ class WarehouseTestSuite {
         // then
         assertThrows(OrderDoesntExistException.class, () -> warehouse.getOrder("5555"));
     }
+    @Test
+    public void testGetOrder() throws OrderDoesntExistException {
+
+        Warehouse warehouse = new Warehouse();
+
+        Order order = new Order("0021");
+        warehouse.addOrder(order);
+
+        Order orderApproved = warehouse.getOrder("0021");
+
+        assertEquals(orderApproved, order);
+    }
 }
